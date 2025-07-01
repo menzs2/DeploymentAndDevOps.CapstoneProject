986245791dd6ce89f43171a06304eea51185bde3
77menzs <77menzs@gmail.com>
2025-07-01 10:08:54 +0200

    InventoryItem class

--- LogiTrack/Models/InventoryItem.cs
+++ LogiTrack/Models/InventoryItem.cs
@@ -2,5 +2,15 @@
 
 public class InventoryItem
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
 
+    public string DisplayInfo()
+    {
+        return $"{Name} - {Description} (Qty: {Quantity}) located at {Location}, last updated on {LastUpdated.ToShortDateString()}";
+    }
 }
